0952bb20c2abf8f5ab9d311a1912307051c63784
miraz66 <mirajulislam0509@gmail.com>
2024-12-01 00:44:17 +0600

    Add MySql questions

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,24 @@
-import {useState} from "react";
+import { useState } from "react";
 import SideBar from "./Components/SideBar.jsx";
-import Main from "./Components/Main.jsx";
+import Home from "./Components/Home.jsx";
 
 function App() {
-  const [questionName, setQuestionName] = useState('react')
-  console.log(questionName)
+  const [questionName, setQuestionName] = useState("react");
+  console.log(questionName);
 
   return (
-      <div className="bg-neutral-950">
-        <div className="max-w-7xl mx-auto grid grid-cols-4 font-display tracking-wide">
-         <div className="col-span-1">
-          <SideBar questionName={questionName} setQuestionName={setQuestionName}/>
-         </div>
-          <Main questionName={questionName} />
+    <div className="bg-neutral-950">
+      <div className="max-w-7xl mx-auto grid grid-cols-4 font-display tracking-wide">
+        <div className="col-span-1">
+          <SideBar
+            questionName={questionName}
+            setQuestionName={setQuestionName}
+          />
         </div>
+        <Home questionName={questionName} />
       </div>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
